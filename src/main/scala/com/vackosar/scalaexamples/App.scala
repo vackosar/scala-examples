1ebb1bd3e4ead4351575ffa84c932f477b940b0d
vackosar <vackosar@gmail.com>
2017-07-08 12:49:55 +0200

    Case class

--- src/main/scala/com/vackosar/scalaexamples/App.scala
+++ src/main/scala/com/vackosar/scalaexamples/App.scala
@@ -1,9 +1,19 @@
 package com.vackosar.scalaexamples
 
-/**
- * Hello world!
- *
- */
-object App extends App {
-  println( "Hello World!" )
+object App {
+
+  def main(args: Array[String]): Unit = {
+    println("Text")
+    val a = A("Alpha")
+    val b = A("Alpha")
+    println(a == b)
+    printHello()
+  }
+
+  def printHello(): Unit = {
+    println("Hello")
+  }
+
+  case class A(a: String)
+
 }
