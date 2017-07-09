573cfee58557e03502a8f87ef22e204437ead3d3
vackosar <vackosar@gmail.com>
2017-07-09 12:14:47 +0200

    FuturesTest

---
+++ src/test/scala/com/vackosar/scalaexamples/FuturesTest.scala
@@ -0,0 +1,24 @@
+package com.vackosar.scalaexamples
+
+import java.util.concurrent.ForkJoinPool
+
+import org.junit.Test
+import org.junit.Assert._
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+@Test
+class FuturesTest {
+
+  @Test
+  def future(): Unit = {
+    implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool())
+    implicit val timeout = FiniteDuration(1, SECONDS)
+    Future[Int]({1 + 2 + 3}).onComplete({
+      case Success(sum) => assertEquals(6, sum)
+      case Failure(_) => fail()
+    })
+  }
+}
